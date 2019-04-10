164fd551c3e59445668f6056dd095aacffbbe5c5
eayalasoto <eduardo.ayala@gisconsultoria.com>
2019-04-09 20:06:13 -0500

    pantallas updated

---
+++ Prosesol/src/main/resources/static/js/custom/datatables-init.js
@@ -0,0 +1,4 @@
+$('.table').DataTable({
+	dom : 'Bfrtip',
+	buttons : [ 'excel' ]
+});	
---
+++ Prosesol/src/main/resources/static/js/custom/form-init.js
@@ -0,0 +1,3 @@
+$('body').addClass("app header-fixed sidebar-fixed aside-menu-fixed sidebar-lg-show");
+$('head').append('<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">');
+//$('.wrap').wrapAll('<div class="app-body" />');
