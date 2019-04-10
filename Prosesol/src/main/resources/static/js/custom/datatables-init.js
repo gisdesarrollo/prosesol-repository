2f813adab1e2d3c4599ac9667b0eeb885d497fca
eayalasoto <eduardo.ayala@gisconsultoria.com>
2019-04-10 10:04:13 -0500

    update 2

--- Prosesol/src/main/resources/static/js/custom/datatables-init.js
+++ Prosesol/src/main/resources/static/js/custom/datatables-init.js
@@ -1,4 +1,19 @@
-$('.table').DataTable({
-	dom : 'Bfrtip',
-	buttons : [ 'excel' ]
+$('table').DataTable({
+    dom: 'Bfrtip',
+    buttons: ['excel']
+//    "language": {
+//        "search": "Buscar:",
+//        "emptyTable": "No hay registros en la tabla",
+//        "info": "Mostrando _START_ a _END_ de _TOTAL_ registros",
+//        "infoEmpty": "Mostrando 0 registros",
+//        "infoFiltered": "(De un total de _MAX_ registros)",
+//        "thousands": ",",
+//        "lengthMenu": "Mostrar _MENU_ registros",
+//        paginate: {
+//            first: 'Primera',
+//            previous: '<',
+//            next: '>',
+//            last: 'Ultima'
+//        }
+//    }
 });	
