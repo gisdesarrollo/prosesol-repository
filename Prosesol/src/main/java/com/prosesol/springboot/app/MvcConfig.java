2f6111915aa2cd334c9ae770cce961dcb430bb46
LuisMorales <luisEnrique@luis.github.com>
2019-05-03 21:37:11 -0500

    Configuración de templates de errores
    Se configuraron los templates para los errores 403 y 404

--- Prosesol/src/main/java/com/prosesol/springboot/app/MvcConfig.java
+++ Prosesol/src/main/java/com/prosesol/springboot/app/MvcConfig.java
@@ -3,15 +3,10 @@ package com.prosesol.springboot.app;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer{
-
-	public void addViewControllers(ViewControllerRegistry registry) {
-		registry.addViewController("/errores/error_403").setViewName("/errores/error_403");
-	}
 	
 	@Bean
 	public BCryptPasswordEncoder passwordEncoder() {
--- Prosesol/src/main/java/com/prosesol/springboot/app/entity/dao/IUsuarioDao.java
+++ Prosesol/src/main/java/com/prosesol/springboot/app/entity/dao/IUsuarioDao.java
@@ -1,5 +1,7 @@
 package com.prosesol.springboot.app.entity.dao;
 
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.prosesol.springboot.app.entity.Usuario;
@@ -8,4 +10,7 @@ public interface IUsuarioDao extends CrudRepository<Usuario, Long>{
 
 	public Usuario findByUsername(String username);
 	
+	@Modifying
+	@Query(value = "update rel_usuarios_perfiles r set r.id_perfil = ?1 where r.id_usuario = ?2", nativeQuery = true)
+	public void updateRelUsuarioPerfil(Long idPerfil, Long idUsuario);
 }
