0550c3df8cc70f49ba4bade9566f13a7bd0715bd
LuisMorales <luisEnrique@luis.github.com>
2019-05-20 19:24:07 -0500

    Cambios 20190520
    Se siguen haciendo pruebas para el envío de correo.

--- Prosesol/src/main/java/com/prosesol/springboot/app/service/EmailServiceImpl.java
+++ Prosesol/src/main/java/com/prosesol/springboot/app/service/EmailServiceImpl.java
@@ -6,13 +6,10 @@ import java.nio.charset.StandardCharsets;
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 
-import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.InputStreamSource;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
@@ -22,11 +19,12 @@ import org.thymeleaf.spring5.SpringTemplateEngine;
 import com.prosesol.springboot.app.entity.Correo;
 import com.prosesol.springboot.app.util.Mail;
 
+
 @Service
 public class EmailServiceImpl implements IEmailService {
 
 	private static final Logger logger = LoggerFactory.getLogger(EmailServiceImpl.class);
-
+	
 	private static String html;
 
 	@Autowired
@@ -34,18 +32,20 @@ public class EmailServiceImpl implements IEmailService {
 
 	@Autowired
 	private SpringTemplateEngine templateEngine;
-	
+
 	@Autowired
 	private CorreoServiceImpl correoService;
-	
+
 	@Autowired
 	private AdjuntoServiceImpl adjuntoService;
 
+	
 	@Override
 	public void sendSimpleMessage(Mail mail, String bandera) throws MessagingException, IOException {
-		
-		Correo correo = new Correo();
 
+		Correo correo = new Correo();
+		templateEngine = new SpringTemplateEngine();
+		
 		MimeMessage message = javaMailSender.createMimeMessage();
 		MimeMessageHelper helper = new MimeMessageHelper(message, MimeMessageHelper.MULTIPART_MODE_MIXED_RELATED,
 				StandardCharsets.UTF_8.name());
@@ -61,18 +61,19 @@ public class EmailServiceImpl implements IEmailService {
 			break;
 		case "inscripcion":
 			logger.info("Template de inscripcion");
-			
+			// Accedemos al iniciador de templates
+//			this.getTemplateEngine();
+
 			correo = correoService.getTemplateCorreoByName(bandera);
 //			adjuntoService.getAdjuntoCorreo(correo.getId());
-			
+
 			html = correo.getHtml();
-			System.out.println(html);
-			
-//			html = this.templateEngine.process("/mail/inscripcion_template", context);
+			html = this.templateEngine.process(html, context);
+
 			break;
 		case "aviso":
 			logger.info("Template de aviso");
-			
+
 			correo = correoService.getTemplateCorreoByName(bandera);
 			html = correo.getHtml();
 			break;
@@ -82,8 +83,6 @@ public class EmailServiceImpl implements IEmailService {
 		helper.setText(html, true);
 		helper.setSubject(mail.getSubject());
 		helper.setFrom(mail.getFrom());
-		
-		
 
 		helper.addInline("suspensionHeader", new ClassPathResource("static/img/logos/suspension_header.jpg"),
 				"image/jpg");
@@ -92,5 +91,5 @@ public class EmailServiceImpl implements IEmailService {
 
 		javaMailSender.send(message);
 	}
-
+	
 }
