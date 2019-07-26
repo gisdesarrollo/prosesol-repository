ad56bfb882ca41cbd9bab3dea463be2f6596b24c
LuisMorales <luisEnrique@luis.github.com>
2019-07-26 10:33:29 -0500

    Respaldo 20190726

--- Prosesol/src/main/java/com/prosesol/springboot/app/entity/dao/IRelServicioBeneficioDao.java
+++ Prosesol/src/main/java/com/prosesol/springboot/app/entity/dao/IRelServicioBeneficioDao.java
@@ -10,7 +10,7 @@ import com.prosesol.springboot.app.entity.rel.RelServicioBeneficio;
 
 public interface IRelServicioBeneficioDao extends CrudRepository<RelServicioBeneficio, Long>{
 
-	@Query(value = "select r.id_servicio, r.id_beneficio, r.descripcion, r.titual, r.beneficiario from rel_servicios_beneficios r where r.id_servicio = ?1", nativeQuery = true)
+	@Query(value = "select r.id_servicio, r.id_beneficio, r.descripcion, r.titular, r.beneficiario from rel_servicios_beneficios r where r.id_servicio = ?1 order by r.id_beneficio", nativeQuery = true)
 	public List<RelServicioBeneficio> getRelServicioBeneficioByIdServicio(Long id);
 	
 	@Modifying
